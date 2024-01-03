78b13666099abb7f16f55479efbf43e710104ca1
MKLepium <maximilian.kurzawski@gmail.com>
2024-01-03 18:55:25 +0100

    fix

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def process_date(date, schedule_df, dev_list, cur):
 
         for index, row in stop_change_df.iterrows():
             logging.debug(f"Processing stop change row: {row}")
-            matching_schedule_rows = schedule_df[schedule_df['stop_id'] == row['stop']]
+            matching_schedule_rows = schedule_df[int(schedule_df['stop_id']) == int(row['stop'])]
             logging.debug(f"Found {len(matching_schedule_rows)} matching schedule rows")
 
 
