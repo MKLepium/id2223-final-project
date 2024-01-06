ba6110a1b5eab716a5b7d55de0de5f7607d6c2ce
William Asp <wille.asp@live.se>
2024-01-06 16:27:25 +0100

    working demo with only ferry the following day

--- UI/main.py
+++ UI/main.py
@@ -4,87 +4,99 @@ import gradio as gr
 import datetime
 import requests
 import xml.etree.ElementTree as ET
+import hopsworks
+import joblib
+import pandas as pd
+from db import DBManager
+import os
 
-MODELNAME = "model_name"
-MODELVERSION = "1.0"
+MODELNAME = "bus_model"
+MODELVERSION = 1
+MODEL_FILE = "model.pkl"
 
-# Dunno if you actually need this method, but here you go
-def get_model(name, version):
-    pass
-
-def predict(input):
-    pass
-
-
-# example input string: 01.10.2023
-# example output datetime: 2023-10-01
-def get_date_from_string(date_str: str):
-    date_format = "%d.%m.%Y"
-    date = datetime.datetime.strptime(date_str, date_format)
-    return date
+DAY_PREFIX = "day_"
+FER_PREFIX = "fer_"
 
 
+# download model
+project = hopsworks.login(project="ID2223_MKLepium")
+mr = project.get_model_registry()
+model_client = mr.get_model(MODELNAME, version=MODELVERSION)
+
+path = os.path.join(MODELNAME, MODEL_FILE)
+if not os.path.exists(path):
+    model_dir = model_client.download()
+model = joblib.load(path)
+print("Model downloaded")
+
+schema = model_client.model_schema
 
 
-def create_row(input):
-    # input is a dict
-    # should have: date, weekday, temp_min, temp_max, temp_avg, fer
-    # extracted from the txt
-    fers = ['7-B', '1-I', 'A1', '4-G', '1-D', '55-A', '1-G', '3-B', '79-A', '22-A', '105-A', '1-H', '88-A', '12-H', '24-A', '17-A', '56-A', '52-A', '1-E', '104-A', '11-E', '89-A', '2-B', '4-E', '12-B', '13-C', '5-B', '18-C', '71-A', '14-E', '2-C', '1-B', '16-B', '3-C', '14-C', '36-A', '15-C', '4-A', '106-A', '15-E', 'A4', '55-B', '3-D', '21-C', '2-E', '57-B', '51-E', 'A6-B', '55-C', '12-F', '3-R', '6-D', '15-D', '16-A', '8-A', '2-G', 'A6', '15-H', '21-A', '15-F', '78-A', '14-D', '12-G', '3-E', '18-A', 'A5', '1A-R', '18-E', '24-C', '11-B', '59-A', '51-B', '28-B', '35-A', '1-A', '13-A', '11-A', '4-B', '55-D', '58-A', '51-A', '17-B', '1-K', '31-B', '6-B', '18-F', '1-F', '82-A', '12-D', '2-F', '1-J', '14-F', '13-E', '11-C', '6-F', '1-L', '19-B', '51-D', '57-D', '101-A', '2-D', '3-A', '7-A', '57-A', '51-C', '14-B', '72-A', 'A2', '19-A', '6-E', '13-B', '12-C', '57-C', '4-F', '5-E', '15-A', '4-C', 'A3', '6-C', '1B-R', '23-A', '18-G', '73-A', '2-A', '12-E', '31-A', '11-D', '21-B', '1-C', '28-A', '11-F', '5-D', '24-B', '81-A', '1-R', '3-F', '15-G', '103-A', '13-D', '6-A', '18-D', '5-C', '5-A', '16-C', '15-B', '14-A', '64-A', '18-B', '4-D', '12-A']
-    
+# transform schema to pandas dataframe
+columns = [column['name'] for column in schema['input_schema']['columnar_schema']]
+columns_df = pd.DataFrame(columns=columns)
+for column in schema['input_schema']['columnar_schema']:
+    columns_df[column['name']] = columns_df[column['name']].astype(column['type'])
 
-    # output should be:
-    # monday (bool), tuesday (bool), wednesday (bool), thursday (bool), friday (bool), saturday (bool), sunday (bool), temp_min, temp_max, temp_avg, and for each fer a bool
-    fer_dict = {}
-    for fer in fers:
-        fer_dict[fer] = False
-
-    row = {
-        "date": input['date'],
-        "monday": input['weekday'] == 0,
-        "tuesday": input['weekday'] == 1,
-        "wednesday": input['weekday'] == 2,
-        "thursday": input['weekday'] == 3,
-        "friday": input['weekday'] == 4,
-        "saturday": input['weekday'] == 5,
-        "sunday": input['weekday'] == 6,
-        "temp_min": 0,
-        "temp_max": 0,
-        "temp_avg": 0,
-    }
-    row.update(fer_dict)
-    # pretty print
-    #for key, value in row.items():
-    #    print(f"\"{key}\": {value},")
-    
-    # adjust row based on the input
-    date = input["date"]
-    weekday = input["weekday"]
-    temp_min = input["temp_min"]
-    temp_max = input["temp_max"]
-    temp_avg = input["temp_avg"]
-    fer = input["fer"]
-
-    row["temp_min"] = temp_min
-    row["temp_max"] = temp_max
-    row["temp_avg"] = temp_avg
-
-    row["monday"] = weekday == 0
-    row["tuesday"] = weekday == 1
-    row["wednesday"] = weekday == 2
-    row["thursday"] = weekday == 3
-    row["friday"] = weekday == 4
-    row["saturday"] = weekday == 5
-    row["sunday"] = weekday == 6
-
-    for fer in fers:
-        row[fer] = fer == input["fer"]
+print("Columns")
+print(columns_df)
 
 
-    #for key, value in row.items():
-    #    print(f"\"{key}\": {value},")
+# download individual ferries
+with DBManager() as db:
+    query = """
+        SELECT DISTINCT fer FROM delay_max_test_v2 ORDER BY fer
+    """
+    cur = db.conn.cursor()
+    cur.execute(query)
+    ferries = cur.fetchall()
+    ferries = [fer[0] for fer in ferries]
 
-    return row
+
+# get dates of training data
+# from start time 2023-10-01 00:00:00 to end time 2023-12-31 23:59:59
+start_date = datetime.datetime(2023, 10, 1, 0, 0, 0)
+end_date = datetime.datetime(2023, 12, 31, 23, 59, 59)
+dates = [("None", None)]
+dates += [
+    (date.strftime("%Y-%m-%d"), date.strftime("%Y-%m-%d")) for date in
+    [start_date + datetime.timedelta(days=x) for x in range(0, (end_date - start_date).days + 1)]
+]
+
+def predict(fer)-> str:
+    """Takes a ferry and a date and returns a delay prediction"""
+    df = columns_df.copy()
+
+    # set all values to false
+    df.loc[0] = False
+    row = df.loc[0]
+
+    # get temp
+    temp_min, temp_max, temp_avg = get_temp_for_tomorrow()
+    row['tmin'] = temp_min
+    row['tmax'] = temp_max
+    row['tavg'] = temp_avg
+
+    # get day name for date
+    today = datetime.datetime.now()
+    tomorrow = today + datetime.timedelta(days=1)
+    day_name = tomorrow.strftime("%A")
+
+    # set day to true
+    day_key = DAY_PREFIX + day_name
+    row[day_key] = True
+
+    # set fer to true
+    fer_key = FER_PREFIX + fer
+    row[fer_key] = True
+
+    # set row in dataframe
+    df.loc[0] = row
+
+    # get prediction
+    prediction = model.predict(df)
+
+    return prediction[0]
 
 def get_weather_forecast():
     url = "https://xmlweather.vedur.is/?op_w=xml&type=forec&lang=en&view=xml&ids=1;27"
@@ -119,7 +131,6 @@ def parse_weather_data(xml_data):
 def get_temp_for_tomorrow():
     weather_data = get_weather_forecast()
     weather_data = parse_weather_data(weather_data)
-    print(len(weather_data))
     today = datetime.datetime.now()
     tomorrow = today + datetime.timedelta(days=1)
     tomorrow_date_string = tomorrow.strftime("%Y-%m-%d")
@@ -129,7 +140,6 @@ def get_temp_for_tomorrow():
         # Check if the forecast's time starts with the date of tomorrow
         if forecast['time'].startswith(tomorrow_date_string):
             weather_data_tomorrow.append(forecast)
-    print(len(weather_data_tomorrow))
     # calculate the average, min and max temperature
     temp_min = 100
     temp_max = -100
@@ -142,9 +152,6 @@ def get_temp_for_tomorrow():
         if temp > temp_max:
             temp_max = temp
     temp_avg = temp_sum / len(weather_data_tomorrow)
-    print(temp_min)
-    print(temp_max)
-    print(temp_avg)
     return temp_min, temp_max, temp_avg
 
 
@@ -152,62 +159,14 @@ def get_temp_for_tomorrow():
 def get_temp_for_date(date):
     pass
 
-def process_input_data(date='2023-10-01', temp_min=-10, temp_max=10, temp_avg=0, fer='7-B', use_tomorrows_weather_forecast=True):
-    # Convert string date to datetime object
-    date = datetime.datetime.strptime(date, '%Y-%m-%d')
-    weekday = date.weekday()
-    # Process data as needed
-    input = {
-        "date": date,
-        "weekday": weekday,
-        "temp_min": temp_min,
-        "temp_max": temp_max,
-        "temp_avg": temp_avg,
-        "fer": fer
-    }
-    row = create_row(input)
-    output = predict(row)
+demo = gr.Interface(
+    fn=predict,
+    inputs=[
+        gr.Dropdown(ferries, label="Select Ferry"),
+        # gr.Dropdown(dates, label="Select Historical Day"),
+    ],
+    outputs=gr.Textbox(label="Delay Information")
+)
 
-    return output
-
-
-"""
-Sample input:
-input = {
-    "date": date,
-    "weekday": date.weekday(),
-    "temp_min": temp_min,
-    "temp_max": temp_max,
-    "temp_avg": temp_avg,
-    "fer": "7-B"
-}
-"""
-if __name__ == "__main__":
-    model = get_model(MODELNAME, MODELVERSION)
-
-    # 01.10.2023
-    date = get_date_from_string("01.10.2023")
-    temp_min, temp_max, temp_avg = get_temp_for_tomorrow()
-
-    """gr.inputs.Date(label="Date"),
-    gr.inputs.Number(label="Minimum Temperature"),
-    gr.inputs.Number(label="Maximum Temperature"),
-    gr.inputs.Number(label="Average Temperature"),
-    gr.inputs.Textbox(label="FER")"""
-    interface = gr.Interface(
-        fn=process_input_data,
-        inputs=[
-            "text",
-            "text",
-            "text",
-            "text",
-            "text",
-            "checkbox"
-        ],
-        examples=[
-            ["2023-10-01", "-10", "10", "0", "7-B", False],
-            ["2024-01-02", "-10", "0", "-8", "7-B", True],
-        ],
-        outputs="json"
-    )
-    interface.launch()
+# Run the Gradio interface
+demo.launch()
